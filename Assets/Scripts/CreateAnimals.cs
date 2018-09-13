53fb7b107bbfc5aa6e2b551ccbeaa094532543fa
aylafernandes <fernandes.ayla@gmail.com>
2018-09-13 00:01:44 -0300

    Criado base para backgroud, inclui os assets dos animais, do et e iniciei o gerador de animais

---
+++ Assets/Scripts/CreateAnimals.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateAnimals : MonoBehaviour {
+
+    public GameObject animal;
+
+    public float limitBottom, limitLeft, limitRight, time;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Create(time));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Create(float t){
+        Vector3 p = new Vector3(Random.Range(limitLeft, limitRight), limitBottom, 0.0f);
+
+        Instantiate(animal, p, animal.transform.rotation);
+
+        yield return new WaitForSeconds(t);
+
+        StartCoroutine(Create(time));
+        
+    }
+}
