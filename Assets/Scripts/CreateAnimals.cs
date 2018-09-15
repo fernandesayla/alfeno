36819f994acb92440f176b397fa5118cdc36625a
aylafernandes <fernandes.ayla@gmail.com>
2018-09-15 20:34:53 -0300

    juntei na cena a vaca com o ufo, porém não consegui fazer a vaca seguir o ufo

--- Assets/Scripts/CreateAnimals.cs
+++ Assets/Scripts/CreateAnimals.cs
@@ -4,29 +4,39 @@ using UnityEngine;
 
 public class CreateAnimals : MonoBehaviour {
 
+    [SerializeField]
+    public Transform target;
     public GameObject animal;
 
-    public float limitBottom, limitLeft, limitRight, time;
+    float originalY;
+    public float limitBottom, limitLeft, limitRight, time,  floatStrength = 1;
 
 	// Use this for initialization
 	void Start () {
         StartCoroutine(Create(time));
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+ 
+
+
 
     IEnumerator Create(float t){
         Vector3 p = new Vector3(Random.Range(limitLeft, limitRight), limitBottom, 0.0f);
 
         Instantiate(animal, p, animal.transform.rotation);
 
+        print(this.target.tag);
+
+
+        animal.GetComponent<Seguir>().SetTarget(this.target);
+
         yield return new WaitForSeconds(t);
 
         StartCoroutine(Create(time));
+
+ 
+
+
         
     }
 }
---
+++ Assets/Scripts/Seguir.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seguir : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float velo = 1;
+
+
+    // Use this for initialization
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        var journey = target.position - this.transform.position;
+        journey = journey.normalized;
+        journey *= this.velo;
+        this.transform.position += journey * Time.deltaTime;
+    }
+
+    public void SetTarget(Transform t)
+    {
+
+        this.target = t;
+    }
+}
