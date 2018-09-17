1f3ad8695fa0cf8800b26989d8776d639cfb6c72
aylafernandes <fernandes.ayla@gmail.com>
2018-09-17 20:59:38 -0300

    script pontuação

---
+++ Assets/Scripts/Pontuacao.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pontuacao : MonoBehaviour {
+    public int pontos;
+
+    [SerializeField]
+    private Text txtPontuacao;
+
+    public  void AdicionarPontuacao(){
+        this.pontos++;
+        this.txtPontuacao.text = this.pontos.ToString(); 
+    }
+}
